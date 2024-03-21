1a57952b1115461eb39dcfba1e6080c3af5578cc
dddochi <86943499+dddochi@users.noreply.github.com>
2024-03-21 21:18:25 +0900

    sorting & searching - selection sort : finish

--- sorting_searching/Main.java
+++ sorting_searching/Main.java
@@ -9,18 +9,39 @@ public class Main {
             arr[i] = scanner.nextInt();
         }
         for(int x : solution(n, arr)){
-            System.out.println(x + " ");
+            System.out.print(x + " ");
         }
     }
+    //혼자 풀어본 코드 - 정답
+    //bubble sort
     public static int[] solution(int n, int[] arr){
-        for(int i = 0; i < arr.length-1; i++){
-            for(int j = 0; j < arr.length - i; j++){
+        for(int i = 0; i < n - 1; i++){
+            boolean isChanged = false;
+            for(int j = 0; j < n -i-1; j++){
                 if(arr[j] > arr[j+1]) {
                     int tmp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = tmp;
+                    isChanged = true;
                 }
             }
+            if(!isChanged) break;
+        }
+        return arr;
+    }
+    //문제 설명 듣고 풀어본 코드
+    //선택 정렬 - selection sort
+    public static int[] solution2(int n, int[] arr){
+        for(int i = 0; i < n - 1; i++){
+            int idx = i;
+            for(int j = i + 1; j < n; j++){
+                if(arr[idx] > arr[j] ){
+                    idx = j;
+                }
+            }
+            int tmp = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = tmp;
         }
         return arr;
     }
