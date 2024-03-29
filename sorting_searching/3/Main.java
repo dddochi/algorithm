ba72a5fdc5ca649352d5eed0fe8b46c53be20931
dddochi <86943499+dddochi@users.noreply.github.com>
2024-03-29 23:26:19 +0900

    sorting - insertion sort : finish

--- sorting_searching/3/Main.java
+++ sorting_searching/3/Main.java
@@ -49,4 +49,16 @@ public class Main {
         }
         return arr;
     }
+    //강사님 코드
+    public static int[] solution3(int n, int arr[]){
+        for(int i = 1; i <n; i++){
+            int tmp = arr[i], j;
+            for(j = i-1; j>=0; j--){
+                if(arr[j] > tmp) arr[j+1] = arr[j];
+                else break;
+            }
+            arr[j+1] = tmp;
+        }
+        return arr;
+    }
 }
