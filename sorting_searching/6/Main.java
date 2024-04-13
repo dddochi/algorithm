ecc8098bda6a94a6b74f626776713dc659ce16ad
dddochi <86943499+dddochi@users.noreply.github.com>
2024-04-13 23:57:13 +0900

    sorting : 6 - change an order with friend illegally : finish

--- sorting_searching/6/Main.java
+++ sorting_searching/6/Main.java
@@ -11,7 +11,9 @@ public class Main {
         for(int x : solution(n, arr)){
             System.out.print(x + " ");
         }
-    }    
+    }  
+    //혼자 풀어본 코드
+    //결과 출력은 잘 되지만, 오답 뜬다..!  
     public static List<Integer> solution(int n, int[] arr){
         List<Integer> answer = new ArrayList<>();
         boolean isChanged = false;
@@ -28,6 +30,34 @@ public class Main {
                 answer.add(idx + 1);
                 break;
             }
+        }
+        return answer;
+    }
+    //설명 듣고 풀어본 코드
+    public static List<Integer> solution2(int n, int[] arr){
+        int[] tmp = new int[n];
+        List<Integer> answer = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            tmp[i] = arr[i];
+        }
+        Arrays.sort(tmp);
+
+        //비교
+        for(int i = 0; i < n; i++){
+            if(tmp[i] != arr[i]){
+                answer.add(i+1);
+            }
+        }
+
+        return answer;
+    }
+    //강사님 코드
+    public static List<Integer> solution3(int n, int[] arr){
+        List<Integer> answer = new ArrayList<>();
+        int [] tmp = arr.clone(); //깊은 복사
+        Arrays.sort(tmp);
+        for(int i = 0; i < n; i++){
+            if(tmp[i] != arr[i]) answer.add(i+1);
         }
         return answer;
     }
