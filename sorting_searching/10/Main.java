37b7224c0036212a7a34133153ddb3203655dd93
dddochi <86943499+dddochi@users.noreply.github.com>
2024-04-25 00:05:01 +0900

    sorting : decision algorithm2 - finish

--- sorting_searching/10/Main.java
+++ sorting_searching/10/Main.java
@@ -13,35 +13,36 @@ public class Main {
         System.out.println(solution(n, c, arr));
     }
     public static int solution(int n, int c, int[] arr){
+        Arrays.sort(arr);
         int lt = 1;
         int rt = arr[n-1];
         int answer = 0;
+        int mid = 0;
+
         while (lt <= rt) {
-            int mid = (lt + rt) / 2;
-            int ep = arr[0];
-            if(count(arr, mid) == c){ //ok 
-                lt = mid + 1;
+            mid = (lt + rt) / 2;
+            if(count(arr, mid) >= c){
+                //범위에 들어오는 값
+                lt = mid + 1; 
                 answer = mid;
             }
-            else if(count(arr, mid) > c){ 
-                lt = mid + 1;
+            else{
+                //범위에 들어오지 x -> 배치하는 말의 개수가 c보다 작음
+                rt = mid - 1;
             }
-            else{//거리가 줄어야함
-                rt = mid -1;
-            }
-            
-        }  
-        return answer;  
+        }
+        return answer;
     }
     public static int count(int[] arr, int mid){
-        int count = 1;
-        int ep = 1;
+        int count = 1; //말의 개수
+        int ep = arr[0]; //말을 놓는 위치 -> 계속 갱신
         for(int i = 1; i < arr.length; i++){
             if(arr[i] - ep >= mid){
-                ep = arr[i];
                 count++;
+                ep = arr[i];
             }
         }
         return count;
     }
+    
 }
