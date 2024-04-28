0775585a1365e850f8c2e5ab6508043d2e6de943
dddochi <86943499+dddochi@users.noreply.github.com>
2024-04-28 23:45:11 +0900

    resursive - 1 : finish

--- recursive_tree_graph/Main.java
+++ recursive_tree_graph/Main.java
@@ -5,11 +5,20 @@ public class Main {
         int n = scanner.nextInt();
         solution(n);
     }
+    //혼자 풀어본 코드
     public static void solution(int n){
         List<Integer> answer = new ArrayList<>();
         if (n > 1) {
             solution(n-1);
         }
         System.out.println(n + " ");
+    }
+    //강사님 코드
+    public static void DFS(int n ){
+        if(n==0) return ; //return; - 함수 종료의 의미
+        else{
+            DFS(n-1);
+            System.out.println(n + " ");
+        }
     }
 }
