c14833033323e0117c67955b153dcf02b3bef643
dddochi <86943499+dddochi@users.noreply.github.com>
2024-05-11 00:41:49 +0900

    binary tree search : BFS : finish

--- recursive_tree_graph/7/Main.java
+++ recursive_tree_graph/7/Main.java
@@ -1,3 +1,8 @@
+//BFS (넓이 우선 탐색) - 레벨 탐색
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Node{
     int data;
     Node lt, rt;
@@ -8,16 +13,7 @@ class Node{
     }
 }
 public class Main {
-    public static void DFS(Node node){
-        if(node.lt == null){ return;}
-        else{
-            if(node.data == 1) System.out.print(node.data + " ");
-            System.out.print(node.lt.data + " ");
-            System.out.print(node.rt.data + " ");
-            DFS(node.lt);
-            DFS(node.rt);
-        }
-    }
+   
     public static void main(String[] args) {
         Node root;
         root = new Node(1);
@@ -27,6 +23,37 @@ public class Main {
         root.lt.rt = new Node(5);
         root.rt.lt = new Node(6);
         root.rt.rt = new Node(7);   
-        DFS(root); 
-    }    
+        BFS(root); 
+    }  
+    
+    //혼자 작성한 코드
+    public static void BFS(Node node){
+        if(node.lt == null){ return;}
+        else{
+            if(node.data == 1) System.out.print(node.data + " ");
+            System.out.print(node.lt.data + " ");
+            System.out.print(node.rt.data + " ");
+            BFS(node.lt);
+            BFS(node.rt);
+        }
+    }
+    //강사님 코드
+    public static void BFS(Node root){
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0; //level
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            System.out.print(L + " : ");
+            for(int i = 0; i < len; i++){
+                Node cur = Q.poll();
+                System.out.print(cur.data + " ");
+                if(cur.lt != null) Q.offer(cur.lt);
+                if(cur.rt != null) Q.offer(cur.rt);
+            }
+            L++;
+            System.out.println();
+        }
+
+    }
 }
