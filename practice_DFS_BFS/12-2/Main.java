416702c6d6c15fbc9f1e93b53f268e4d2e16a16d
dddochi <86943499+dddochi@users.noreply.github.com>
2024-05-27 00:55:07 +0900

    tomato : BFS - 12 : finish

--- practice_DFS_BFS/12-2/Main.java
+++ practice_DFS_BFS/12-2/Main.java
@@ -1,16 +1,13 @@
-//혼자 풀어본 코드
-//예시 출력 잘됨
-//채점 결과 정답
+//강사님 코드
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static int[][] board, dis;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int n, m;
+    static Queue<Point> Q = new LinkedList<>();
     static class Point{
         private int x;
         private int y;
@@ -20,32 +17,17 @@ public class Main {
         }
     }
     public static void BFS(){
-        Queue<Point> Q = new LinkedList<>();
-        //board[][] == 1인 곳을 찾아서 insert
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(board[i][j] == 1){
-                    Q.offer(new Point(i, j));
-                }
-            }
-        }
-        int L = 1;
         while (!Q.isEmpty()) {
-            int len = Q.size();
-            for(int i = 0; i < len; i++){
-                Point tmp = Q.poll();
+            Point tmp = Q.poll();
                 for(int j = 0; j < 4; j++){
                     int nx = tmp.x + dx[j];
                     int ny = tmp.y + dy[j];
                     if(nx >=0 && nx <n && ny >= 0 && ny < m && board[nx][ny] == 0){
-                        board[nx][ny] = L;
-                        dis[nx][ny] = dis[tmp.x][tmp.y] + 1;
+                        board[nx][ny] = 1;
                         Q.offer(new Point(nx, ny));
+                        dis[nx][ny] = dis[tmp.x][tmp.y] + 1;
                     }
-
-            }
-            }
-            L++;
+                }   
         }
     }
     public static void main(String[] args) {
@@ -55,13 +37,11 @@ public class Main {
 
         board = new int[n][m];
         dis = new int[n][m];
-        int max = 0;
-        int min = 0;
+    
         for(int i =0; i <n; i++){ 
             for(int j = 0; j < m; j++){
                 board[i][j] = scanner.nextInt();
-                if(board[i][j] > max) max = board[i][j];
-                if(board[i][j] < min) min = board[i][j];
+                if(board[i][j] == 1) Q.offer(new Point(i, j));
             }
         }
         if(max == 0 && min == 0) {
@@ -69,28 +49,25 @@ public class Main {
             return;
         }
         BFS();
-        max = 0;
+        boolean flag = true;
+        int answer = Integer.MIN_VALUE;
         for(int i =0; i <n; i++){
             for(int j = 0; j < m; j++){
                 if(max < dis[i][j]) max = dis[i][j];
-                if(board[i][j] == 0){// 토마토가 다 익지 못하는 상황
-                    System.out.println(-1);
-                    return;
-                }
+                if(board[i][j] == 0) flag = false; // 토마토가 다 익지 못하는 상황
+                   
             }
         }
-        System.out.println(max);
-        // for(int i =0; i <n; i++){
-        //     for(int j = 0; j < m; j++){
-        //         System.out.print(board[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
+        if(flag){
+            for(int i =0; i <n; i++){
+                for(int j = 0; j < m; j++){
+                    answer = Math.max(answer, dis[i][j]);
+                }
+            }
+            System.out.println(answer);
+        }
+        else System.out.println(-1);
+        
     }
-
 }
-// 6 4
-// 0 0 -1 0 0 1
-// 0 0 1 0 -1 0
-// 1 0 -1 0 0 0
-// 0 0 0 0 -1 1
+
